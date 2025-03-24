1796ff1c98672713af16e472d3791b795567bc98
Peter Lay <peterlaywaugh@gmail.com>
2025-03-24 18:21:00 +0000

    Run seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+green_tea = Product.create!(code: 'GR1', name: 'Green Tea', price: 3.11)
+strawberries = Product.create!(code: 'SR1', name: 'Strawberries', price: 5.00)
+coffee = Product.create!(code: 'CF1', name: 'Coffee', price: 11.23)
+
+green_tea.discount_rules.create!(
+  name: 'Buy one get one free for Green Tea',
+  rule_type: 'bogof',
+  min_quantity: 2,
+  free_quantity: 1
+)
+
+strawberries.discount_rules.create!(
+  name: 'Bulk strawberry discount',
+  rule_type: 'bulk_price',
+  min_quantity: 3,
+  new_unit_price: 4.50
+)
+
+coffee.discount_rules.create!(
+  name: 'Coffee volume discount',
+  rule_type: 'percent_discount',
+  min_quantity: 3,
+  discount_percentage: 33.33
+)
