75b5f191006055f994299467a00f83a3114a78ad
Peter Lay <peterlaywaugh@gmail.com>
2025-03-24 18:02:46 +0000

    Add model validations

--- app/models/discount_rule.rb
+++ app/models/discount_rule.rb
@@ -1,3 +1,7 @@
 class DiscountRule < ApplicationRecord
   belongs_to :product
+
+  validates :name, presence: true
+  validates :rule_type, presence: true, inclusion: { in: [ "bogof", "bulk_price", "percent_discount" ] }
+  validates :min_quantity, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :discount_rules, dependent: :destroy
+
+  validates :code, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
